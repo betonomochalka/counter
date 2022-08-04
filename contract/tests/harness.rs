b47e33bb893d02d82931a94140d871a107d0d5fb
Egor <zxcnobrainn@gmail.com>
2022-08-04 17:39:35 +0300

    test for decrement

--- contract/tests/harness.rs
+++ contract/tests/harness.rs
@@ -1,18 +1,18 @@
 use fuels::{prelude::*, tx::ContractId};
 
 // Load abi from json
-abigen!(MyContract, "out/debug/contract-abi.json");
+abigen!(MyContract, "/home/admin/counter1.0/contract/out/debug/Pisya-abi.json");
 
 async fn get_contract_instance() -> (MyContract, ContractId) {
     // Launch a local network and deploy the contract
     let wallet = launch_provider_and_get_wallet().await;
 
     let id = Contract::deploy(
-        "./out/debug/contract.bin",
+        "/home/admin/counter1.0/contract/out/debug/Pisya.bin",
         &wallet,
         TxParameters::default(),
         StorageConfiguration::with_storage_path(Some(
-            "./out/debug/contract-storage_slots.json".to_string(),
+            "/home/admin/counter1.0/contract/out/debug/Pisya-storage_slots.json".to_string(),
         )),
     )   
     .await
@@ -33,8 +33,20 @@ async fn can_increment() {
         .await
         .unwrap();
 
-    assert_eq!(1, result.value);
-    println!("{}", result.value);
+    assert_eq!(2, result.value);
     // Now you have an instance of your contract you can use to test each function
 }
 
+#[tokio::test]
+async fn can_decrement() {
+    let (instance, _id) = get_contract_instance().await;
+
+    let result =  instance
+        .decrement_bank()
+        .call()
+        .await
+        .unwrap();
+
+    assert_eq!(0, result.value);
+    // Now you have an instance of your contract you can use to test each function
+}
