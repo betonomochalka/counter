ee22b4b43d4a727a07da433b67c628695165f51c
Egor <zxcnobrainn@gmail.com>
2022-08-16 17:38:49 +0300

    new vite

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,22 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
 import './App.css'
+import { Wallet } from "fuels";
+import { PisyaAbi__factory } from "./types";
+import { count } from 'console';
+
+const contractId = "0x...";
+const wallet = new Wallet("0x...");
+const contract = PisyaAbi__factory.connect(contractId, wallet);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    async function main() {
+      const { value } = await contract.functions.counter().get();
+      setCounter(Number(value));
+    }
+    main();
+  }, []);
+
+  async function increment_bank() {
+    const { value } = await contract.functions.increment_bank(1).call();
+    setCounter(Number(value));
+  }
+
+  async function decrement_bank() {
+    const { value } = await contract.functions.decrement_bank(1).call();
+    setCounter(Number(value));
+  }
+
 
   return (
     <div className="App">
-      <div>
-        <img src="/home/admin/counter/vite/src/assets/vite.webp" className="logo" alt="Kakayouto Pisya" />
-      </div>
-      <h1>Pisya</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <header className="App-header">
+        <p>Counter: {counter}</p>
+        <button onClick={increment_bank}>Increment</button>
+        <button onClick={decrement_bank}>Decrement</button>
+      </header>
     </div>
-  )
+  );
 }
 
 export default App
--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  build: {
+    target: 'es2020'
+  },
+  plugins: [react()],
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  },
+  optimizeDeps: {
+    esbuildOptions:
+     {target: 'es2020'}
+  }
 })
