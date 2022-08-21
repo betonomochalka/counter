445eaf66e7b5e2922c1bf06af1b7ed8f41e41cba
Egor <zxcnobrainn@gmail.com>
2022-08-21 22:16:02 +0300

    update vite

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,18 +1,50 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Wallet } from "fuels";
+import "./App.css";
+import { PisyaAbi__factory } from "./types";
+
+
+const WALLET_SECRET =
+  "0xde97d8624a438121b86a1956544bd72ed68cd69f2c99555b08b1e8c51ffd511c";
+
+const CONTRACT_ID =
+  "0x1e2d6699894311824dde09ad77be539f7fc3a2ea72f61f839238298c87e8da0a";
+
+const wallet = new Wallet(WALLET_SECRET);
+
+const contract = PisyaAbi__factory.connect(CONTRACT_ID, wallet);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    async function main() {
+      const { value } = await contract.functions.get_bank().get();
+      setCount(Number(value));
+    }
+    main();
+  }, []);
+
+  async function increment() {
+    const { value } = await contract.functions.increment_bank().call();
+    setCount(Number(value));
+  }
+
+  async function decrement() {
+    const { value } = await contract.functions.decrement_bank().call();
+    setCount(Number(value));
+  }
 
   return (
     <div className="App">
-      <div>
-        <img src="/home/admin/counter/vite/src/assets/vite.webp" className="logo" alt="Kakayouto Pisya" />
-      </div>
       <h1>Pisya</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <p>{count}</p>
+        <button onClick={increment}>
+          Increment
+        </button>
+        <button onClick={decrement}>
+          Decrement
         </button>
       </div>
     </div>
--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  build: {
+    target: 'es2020'
+  },
+  plugins: [react()],
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  },
+  optimizeDeps: {
+    esbuildOptions:
+     {target: 'es2020'}
+  }
+}) 
